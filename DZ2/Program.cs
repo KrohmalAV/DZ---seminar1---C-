a669af38a3bee7088ede3de0811cefd4d4282ccb
KrohmalAV <Anton.avk@yandex.ru>
2022-11-20 15:57:21 +0300

    Add DZ2 - поиск  максимального числа из трех введенны

---
+++ DZ2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход
+три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Clear();
+Console.Write("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(a == b && a == c)
+{
+    Console.WriteLine($"Все три введенных числа равны. Выделить максимальное невозможно.");
+}
+else
+{
+    if(b > max)
+    {
+        max = b;
+    }
+    if(c > max)
+    {
+        max = c;
+    }
+    Console.WriteLine($"Максимальное из введенных чисел = {max}");
+}
