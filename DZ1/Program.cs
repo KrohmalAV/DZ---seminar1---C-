ec147f9105371df69769bd3bd1abcdcf59ac227b
KrohmalAV <Anton.avk@yandex.ru>
2022-11-20 15:36:59 +0300

    Add DZ1 - поиск большего и меньшего числа

---
+++ DZ1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Clear();
+Console.Write("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine($"Max число = {a}, min число = {b}");
+}
+if(a < b)
+{
+    Console.WriteLine($"Max число = {b}, min число = {a}");
+}
+else
+{
+    Console.WriteLine("Введенные числа равны");
+}
+
+
+
