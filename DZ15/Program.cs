81dbd73ab0f3a004e2a28ef83a1cc17982f5bd37
KrohmalAV <Anton.avk@yandex.ru>
2022-12-03 22:50:18 +0300

    Сумма цифр в числе (через метод)

---
+++ DZ15/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int SumNumber(int A)
+{
+    int result = 0;
+        while(A > 0)
+    {
+        result += A % 10;
+        A = A / 10;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число : ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {A} равняется {SumNumber(A)}");
